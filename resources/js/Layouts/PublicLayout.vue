c338356a8e605e974697aae52d8e3d9ad877fe85
Marvin Deuschle <marvdeus@gmail.com>
2023-02-03 20:34:53 +0100

    finished Imprint.vue

--- resources/js/Layouts/PublicLayout.vue
+++ resources/js/Layouts/PublicLayout.vue
@@ -1,5 +1,6 @@
 <template>
-    <main class="container mx-auto bg-white rounded-2xl p-4 pt-6 transition-all duration-300 md:pt-12">
+    <main
+        class="w-[85vw] mx-auto bg-white rounded-2xl p-4 pt-6 transition-all duration-300 md:pt-12 md:w-[88vw] lg:w-[85vw] xl:w-[80vw] 2xl:w-[75vw]">
         <slot/>
     </main>
 </template>
