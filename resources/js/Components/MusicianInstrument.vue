8b642d24ef48882f14bb0f9be17c38c1596fa71b
Marvin Deuschle <marvdeus@gmail.com>
2023-02-15 19:10:58 +0100

    started to fix the styling issues

--- resources/js/Components/MusicianInstrument.vue
+++ resources/js/Components/MusicianInstrument.vue
@@ -2,8 +2,8 @@
     <h1 class="pt-2 border-t border-gray-300 text-center mb-2 text-2xl md:text-3xl lg:text-4xl">{{ instrument }}</h1>
 
     <div class="flex flex-col justify-evenly mb-2 gap-8 md:flex-row md:flex-wrap">
-        <div v-for="musician in musicians" class="block text-center max-w-[108px] sm:w-[45%] md:w-[30%] lg:px-6">
-            <img class="h-36 aspect-[0.75]" :src="`storage/images/musicians/${musician.picture}`" :alt="musician.name"/>
+        <div v-for="musician in musicians" class="block text-center max-w-[108px] sm:w-[45%] md:w-[30%] lg:px-4">
+            <img :src="musician.picture" :alt="musician.name"/>
             <p class="text-center">{{ musician.name }}</p>
         </div>
     </div>
@@ -20,3 +20,11 @@ const props = defineProps<{
     musicians: Musician[]
 }>()
 </script>
+
+<style scoped>
+img {
+    @apply h-36;
+    aspect-ratio: 0.75;
+    max-width: 100%;
+}
+</style>
--- resources/js/Pages/Band/MusiciansPage.vue
+++ resources/js/Pages/Band/MusiciansPage.vue
@@ -36,38 +36,45 @@ import Heading from "@/Components/Heading.vue";
 import NavLink from "@/Components/Link/NavLink.vue";
 import MusicianInstrument, {Musician} from "@/Components/MusicianInstrument.vue";
 
-// TODO: Add pictures of musicians
+const picture_prefix = 'assets/musician_pictures/';
+const default_tux = picture_prefix + 'default/tux.png';
+const default_tux_sax = picture_prefix + 'default/tux-sax.jpg';
+const default_tux_trumpet = picture_prefix + 'default/tux-trompeter.png';
+const default_tux_guitar = picture_prefix + 'default/tux-gitarrist.png';
+// const default_tux_drum = picture_prefix + 'default/tux-drummer.png';
+// const default_tux_conductor = picture_prefix + 'default/tux-dirigent.png';
+
 const bandleader: Musician[] = [
-    {name: 'Klaus Gehrlein', picture: ''}
+    {name: 'Klaus Gehrlein', picture: picture_prefix + 'gehrlein-klaus.jpg'}
 ]
 const vocals: Musician[] = [
-    {name: 'Gabi Kipper', picture: '',},
-    {name: 'Phillip Leschhorn', picture: ''}
+    {name: 'Gabi Kipper', picture: picture_prefix + 'kipper-gabi.jpeg',},
+    {name: 'Phillip Leschhorn', picture: default_tux}
 ]
 const saxophones: Musician[] = [
-    {name: 'Jutta Acker', picture: ''},
-    {name: 'Niklas Piening', picture: ''},
-    {name: 'Karin Kolb', picture: ''},
-    {name: 'Edi Strobel', picture: ''},
-    {name: 'Jürgen Höppchen', picture: ''},
-    {name: 'Heiko Lübben', picture: ''},
-    {name: 'Sabine Kolb', picture: ''},
+    {name: 'Jutta Acker', picture: picture_prefix + 'acker-jutta.jpeg'},
+    {name: 'Niklas Piening', picture: default_tux_sax},
+    {name: 'Karin Kolb', picture: default_tux_sax},
+    {name: 'Edi Strobel', picture: default_tux_sax},
+    {name: 'Jürgen Höppchen', picture: default_tux_sax},
+    {name: 'Heiko Lübben', picture: picture_prefix + 'luebben-heiko.jpeg'},
+    {name: 'Sabine Kolb', picture: default_tux_sax},
 ]
 const trombones: Musician[] = [
-    {name: 'Michael Blessing', picture: ''},
-    {name: 'Reinhold Paul', picture: ''},
-    {name: 'Jürgen Illers', picture: ''},
+    {name: 'Michael Blessing', picture: default_tux},
+    {name: 'Reinhold Paul', picture: default_tux},
+    {name: 'Jürgen Illers', picture: default_tux},
 ]
 const trumpets: Musician[] = [
-    {name: 'Ursula Tremel', picture: ''},
-    {name: 'Jürgen Scheidt', picture: ''},
-    {name: 'Steffen Kolb', picture: ''},
-    {name: 'Marvin Deuschle', picture: ''},
-    {name: 'Benedikt Sommer', picture: ''},
+    {name: 'Ursula Tremel', picture: picture_prefix + 'tremel-ursel.jpeg'},
+    {name: 'Jürgen Scheidt', picture: picture_prefix + 'scheid-juergen.jpeg'},
+    {name: 'Steffen Kolb', picture: default_tux_trumpet},
+    {name: 'Marvin Deuschle', picture: picture_prefix + 'deuschle-marvin.jpeg'},
+    {name: 'Benedikt Sommer', picture: default_tux_trumpet},
 ]
 const rhythms: Musician[] = [
-    {name: 'Rudi Kolbinger (g)', picture: ''},
-    {name: 'Hort Keller (p)', picture: ''},
-    {name: 'Rudolf Schultz (bass)', picture: ''},
+    {name: 'Rudi Kolbinger (g)', picture: picture_prefix + 'kolbinger-rudi.jpeg'},
+    {name: 'Hort Keller (p)', picture: picture_prefix + 'keller-horst.jpeg'},
+    {name: 'Rudolf Schultz (bass)', picture: default_tux_guitar},
 ]
 </script>
