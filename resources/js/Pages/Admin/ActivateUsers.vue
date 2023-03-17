067adc8b1f3d2ea8ec0e616125d40b9aa3a26af0
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2023-03-17 16:51:19 +0100

    added a layout to ActivateUsers.vue

--- resources/js/Pages/Admin/ActivateUsers.vue
+++ resources/js/Pages/Admin/ActivateUsers.vue
@@ -1,27 +1,36 @@
 <template>
-    <table>
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="user in users">
-            <td>{{ user.id }}</td>
-            <td>{{ user.name }}</td>
-            <td>
-                <Link :href="'/admin/activate-users/'+user.id">Aktivieren</Link>
-            </td>
-        </tr>
-        </tbody>
-    </table>
+    <PublicLayout>
+        <Head>
+            <title>Admin - User aktivieren</title>
+        </Head>
+        <Heading>User aktivieren</Heading>
 
+        <table class="mx-auto w-1/3">
+            <thead>
+            <tr>
+                <th class="w-1/5">ID</th>
+                <th class="w-2/5">Name</th>
+                <th class="w-2/5">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="user in users">
+                <td class="w-1/5">{{ user.id }}</td>
+                <td class="w-2/5">{{ user.name }}</td>
+                <td class="w-2/5">
+                    <Link :href="'/admin/activate-users/'+user.id">Aktivieren</Link>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </PublicLayout>
 </template>
 
 <script lang="ts" setup>
-import {computed, defineProps} from 'vue';
-import {Link} from '@inertiajs/vue3';
+import {defineProps} from 'vue';
+import {Head, Link} from '@inertiajs/vue3';
+import PublicLayout from "@/Layouts/PublicLayout.vue";
+import Heading from "@/Components/Heading.vue";
 
 const props = defineProps<{
     users: {
@@ -34,4 +43,9 @@ console.debug(props.users);
 
 </script>
 
-<style scoped></style>
+<style scoped>
+td, th {
+    border-bottom: 1px solid #999;
+    @apply text-center py-2;
+}
+</style>
