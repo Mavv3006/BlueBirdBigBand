7c33c76e498743c8426127c07101bc26e09b1e8c
Marvin Deuschle <marvdeus@gmail.com>
2023-04-10 15:43:55 +0200

    added deleting musicians and added some logging

--- app/Http/Controllers/MusiciansController.php
+++ app/Http/Controllers/MusiciansController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreMusicianRequest;
 use App\Http\Requests\UpdateMusicianRequest;
 use App\Models\Musician;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -14,13 +17,13 @@ class MusiciansController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
         $musicians = Musician::with('instrument')
             ->orderBy('instrument_id')
             ->orderBy('part')
             ->get();
-        Log::debug('all musicians', [$musicians]);
+        Log::info('Showing all musicians', [$musicians]);
         return Inertia::render(
             'Admin/MusicianManagement/MusiciansIndex',
             ['data' => $musicians]
@@ -49,7 +52,10 @@ class MusiciansController extends Controller
     public function show(Musician $musician): Response
     {
         $instrument = $musician->instrument()->first();
-        Log::debug('showing musician', [$musician, $instrument]);
+        Log::info('Showing musician', [$musician]);
+        $musician_name = $musician->firstname . " " . $musician->lastname;
+        $debug_message = 'The instrument of musician ' . $musician_name . ' (' . $musician->id . ')';
+        Log::debug($debug_message, [$instrument]);
         return Inertia::render(
             'Admin/MusicianManagement/MusiciansShow',
             ['musician' => $musician, 'instrument' => $instrument]
@@ -59,9 +65,10 @@ class MusiciansController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Musician $musician)
+    public function edit(Musician $musician): Redirector|RedirectResponse|Application
     {
-        return "hi";
+        Log::info('Editing musician', [$musician]);
+        return redirect(route('musicians.show', $musician->id));
     }
 
     /**
@@ -75,8 +82,10 @@ class MusiciansController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Musician $musician)
+    public function destroy(Musician $musician): Redirector|RedirectResponse|Application
     {
-        //
+        Log::info('Deleting musician', [$musician]);
+        $musician->delete();
+        return redirect(route('musicians.index'));
     }
 }
