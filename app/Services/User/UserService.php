2b2eb2b0ee3ef4bccba060e40e4658115adf1384
Marvin Deuschle <marvdeus@gmail.com>
2023-06-21 14:18:20 +0200

    refactored AssignRolesToUserController.php

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -2,9 +2,12 @@
 
 namespace App\Services\User;
 
+use App\DataTransferObjects\UserRoleDto;
+use App\Http\Requests\UserRoleRequest;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Log;
+use Spatie\Permission\Models\Role;
 
 class UserService
 {
@@ -19,5 +22,38 @@ class UserService
     {
         $user->update(['activated' => true]);
         Log::debug('Updated user ' . $user->id);
+    }
+
+    public function findByUsername(string $username): User
+    {
+        return User::firstWhere('name', $username);
+    }
+
+    /**
+     * @param User $user
+     * @return UserRoleDto[]
+     */
+    public function getRolesOfUser(User $user): array
+    {
+        $userRoles = $user->getRoleNames();
+
+        $roleMap = Role::all()->map(function ($item) use ($userRoles) {
+            return new UserRoleDto($item->id, $item->name, $userRoles->contains($item->name));
+        });
+
+        Log::debug(json_encode($roleMap));
+        return $roleMap;
+    }
+
+    public function syncRoles(User $user, UserRoleRequest $request): void
+    {
+        $data = $request->validated();
+
+        Log::debug(json_encode($data));
+        $user->syncRoles($data['roles']);
+        Log::info('Updating the role assignment for a user', [
+            'user' => ['id' => $user->id, 'name' => $user->name],
+            'roles' => $data['roles']
+        ]);
     }
 }
