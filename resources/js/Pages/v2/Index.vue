d87a4364ca39597b69ba033c30d0717ea67f4441
Marvin Deuschle <marvdeus@gmail.com>
2023-08-11 17:50:25 +0200

    refactored the layout into its seperate layout file

--- resources/js/Pages/v2/Index.vue
+++ resources/js/Pages/v2/Index.vue
@@ -1,37 +1,15 @@
 <template>
-  <div class="wrapper">
-    <div class="content">
-      <Header/>
-      <IndexConcertList :concerts="upcomingConcerts"/>
-    </div>
-
-    <div class="footer">
-      <Footer/>
-    </div>
-  </div>
+  <PublicLayout page-title="Willkommen">
+    <IndexConcertList :concerts="upcomingConcerts"/>
+  </PublicLayout>
 </template>
 
 <script lang="ts" setup>
-import Header from '@/Components/v2/Navigation/Header.vue';
-import Footer from '@/Components/v2/Footer/Footer.vue';
 import IndexConcertList from "@/Components/v2/Concert/IndexConcertList.vue";
 import {Concert} from "@/types/concert";
+import PublicLayout from "@/Layouts/v2/PublicLayout.vue";
 
 defineProps<{
   upcomingConcerts: Concert[]
 }>();
 </script>
-
-<style scoped>
-.wrapper {
-  @apply flex flex-col h-screen;
-}
-
-.content {
-  flex: 1 0 auto;
-}
-
-.footer {
-  flex-shrink: 0;
-}
-</style>
