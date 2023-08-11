9035cf801bc5bb8d30c63fc00f0001cdcc531ce7
Marvin Deuschle <marvdeus@gmail.com>
2023-08-11 17:29:24 +0200

    added upcoming concert section to index page

---
+++ resources/js/Components/v2/Concert/IndexConcertList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-white">
+    <SectionHeader class="pl-16 pt-8">Unsere nächsten Auftritte</SectionHeader>
+
+    <div class="flex justify-evenly mt-5 pb-20">
+      <Concert v-for="concert in concerts" :data="concert"/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {Concert as ConcertType} from "@/types/concert";
+import Concert from "@/Components/v2/Concert/Concert.vue";
+import SectionHeader from "@/Components/v2/SectionHeader.vue";
+
+defineProps<{
+  concerts: ConcertType[]
+}>();
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/v2/Index.vue
+++ resources/js/Pages/v2/Index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="bg-gray-800 h-screen">
     <Header/>
+
+    <!--    <div class="w-full h-28 bg-gray-500"></div>-->
+
+    <IndexConcertList :concerts="upcomingConcerts"/>
+
     <Footer/>
   </div>
 </template>
@@ -8,6 +13,12 @@
 <script lang="ts" setup>
 import Header from '@/Components/v2/Navigation/Header.vue';
 import Footer from '@/Components/v2/Footer/Footer.vue';
+import IndexConcertList from "@/Components/v2/Concert/IndexConcertList.vue";
+import {Concert} from "@/types/concert";
+
+defineProps<{
+  upcomingConcerts: Concert[]
+}>();
 </script>
 
 <style scoped>
