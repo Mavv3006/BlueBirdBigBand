b9cdfb73439bc4bb02d680fa25b3967e1db8fecd
Marvin Deuschle <marvdeus@gmail.com>
2023-08-21 20:14:55 +0200

    1st pre-commit run

--- app/View/Components/Layouts/FooterNavigation.php
+++ app/View/Components/Layouts/FooterNavigation.php
@@ -2,7 +2,6 @@
 
 namespace App\View\Components\Layouts;
 
-use App\Services\View\NavigationLinksService;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
--- bin/pre-commit/pint.sh
+++ bin/pre-commit/pint.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 ./vendor/bin/pint --dirty
