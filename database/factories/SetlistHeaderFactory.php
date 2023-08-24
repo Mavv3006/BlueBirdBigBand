69f70d49c044edf3f6a71b46eca3150b7da754c3
Marvin Deuschle <marvdeus@gmail.com>
2023-08-24 12:04:36 +0200

    corrected SetlistHeaderFactory.php

--- database/factories/SetlistHeaderFactory.php
+++ database/factories/SetlistHeaderFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Models\Concert;
 use App\Models\SetlistHeader;
-use App\Models\Song;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
@@ -16,8 +15,6 @@ class SetlistHeaderFactory extends Factory
     {
         return [
             'concert_id' => Concert::all()->random(1)->first(),
-            'song_id' => Song::all()->random(1)->first(),
-            'sequence_number' => $this->faker->randomNumber(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
