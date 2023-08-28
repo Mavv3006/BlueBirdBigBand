7d5eb892f59c968e12ff0e3565be6a512fe66882
Marvin Deuschle <marvdeus@gmail.com>
2023-08-28 20:19:12 +0200

    fixed homepage bug and removed unused vue files

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -17,7 +17,7 @@ class PublicController extends Controller
 
     public function home(): Response
     {
-        return Inertia::render('Concert');
+        return Inertia::render('Index');
     }
 
     public function aboutUs(): Response
