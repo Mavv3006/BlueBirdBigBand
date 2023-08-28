64e9e4769bb7662ebb3cf5ece6c11f485dd923dd
Marvin Deuschle <marvdeus@gmail.com>
2023-08-28 20:40:00 +0200

    added an about us section to the index page

--- resources/views/components/page-sections/concert-listing.blade.php
+++ resources/views/components/page-sections/concert-listing.blade.php
@@ -1,13 +1,13 @@
 <div>
-    <div class="mx-auto">
-        <x-page-sections.section-header class="pl-16 pt-8 pb-4">
+    <div class="mx-auto container">
+        <x-page-sections.section-header class="pb-4">
             {{ $heading }}
         </x-page-sections.section-header>
 
-        <div class="flex flex-col items-center gap-8 pb-8
+        <div class="flex flex-col items-center gap-x-4 gap-y-8
                 md:flex-row md:flex-wrap md:justify-center
-                lg:mx-8 lg:gap-x-16
-                xl:justify-evenly">
+                lg:gap-x-8
+                xl:gap-x-16 xl:justify-evenly">
             @foreach($listOfConcerts as $concert)
                 <x-concert :concert="$concert"/>
             @endforeach
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,13 @@
 <x-layouts.public-layout title="Hello World">
-
     <x-page-sections.hero/>
 
-    <x-page-sections.concert-listing heading="Unsere nächsten Auftritte" :type="\App\Enums\ConcertListingType::Upcoming"
-                                     limit="3"/>
+    <div class="flex gap-8 flex-col mt-8">
+    <x-page-sections.concert-listing
+        heading="Unsere nächsten Auftritte"
+        :type="\App\Enums\ConcertListingType::Upcoming"
+        limit="3"/>
+
+    <x-page-sections.about/>
+    </div>
 
 </x-layouts.public-layout>
