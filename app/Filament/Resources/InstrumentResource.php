bc2c104798231113b4f8bf2d8a3536a1b469836b
Mavv3006 <Mavv3006@users.noreply.github.com>
2023-11-15 18:27:52 +0000

    PHP Linting (Pint)

--- app/Filament/Resources/InstrumentResource.php
+++ app/Filament/Resources/InstrumentResource.php
@@ -20,7 +20,7 @@ class InstrumentResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
+                Forms\Components\TextInput::make('name'),
             ]);
     }
 
