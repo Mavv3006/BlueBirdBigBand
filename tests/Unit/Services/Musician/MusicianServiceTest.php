f60560b9e66fc7408d9a199c3610c79cef09bef2
Marvin Deuschle <marvdeus@gmail.com>
2023-11-21 20:01:00 +0100

    changed musician service

--- tests/Unit/Services/Musician/MusicianServiceTest.php
+++ tests/Unit/Services/Musician/MusicianServiceTest.php
@@ -76,4 +76,32 @@ class MusicianServiceTest extends TestCase
         $this->assertTrue($second['id'] === 1);
         $this->assertTrue($third['id'] === 2);
     }
+
+    public function test_ordering_instruments()
+    {
+        $firstInstrument = Instrument::factory()->create(['name' => 'test1', 'order' => 2]);
+        $secondInstrument = Instrument::factory()->create(['name' => 'test2', 'order' => 1]);
+        $thirdInstrument = Instrument::factory()->create(['name' => 'test3', 'order' => 3]);
+
+        Musician::factory()
+            ->for($secondInstrument)
+            ->create();
+        Musician::factory()
+            ->for($thirdInstrument)
+            ->create();
+        Musician::factory()
+            ->for($firstInstrument)
+            ->create();
+
+        $all = $this->service->activeMusicians();
+
+        $first = $all[0];
+        $second = $all[1];
+        $third = $all[2];
+        $this->assertTrue($first['instrument']['order'] < $second['instrument']['order']);
+        $this->assertTrue($second['instrument']['order'] < $third['instrument']['order']);
+        $this->assertTrue($first['instrument']['id'] === 2);
+        $this->assertTrue($second['instrument']['id'] === 1);
+        $this->assertTrue($third['instrument']['id'] === 3);
+    }
 }
