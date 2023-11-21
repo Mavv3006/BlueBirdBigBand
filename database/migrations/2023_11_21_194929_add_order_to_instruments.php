0c3745b677530692cd9804f63fd9e07ce004a64c
Marvin Deuschle <marvdeus@gmail.com>
2023-11-21 19:51:34 +0100

    created migration to add order column to instruments

---
+++ database/migrations/2023_11_21_194929_add_order_to_instruments.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('instruments', function (Blueprint $table) {
+            $table->unsignedMediumInteger('order')
+                ->unique()
+                ->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('instruments', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+    }
+};
