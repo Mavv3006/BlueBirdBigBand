212e02523232e0c9f35f06aa1c34029c2dc60751
Marvin Deuschle <marvdeus@gmail.com>
2023-11-27 20:58:07 +0100

    added view action to musician resource

--- app/Filament/Resources/MusicianResource.php
+++ app/Filament/Resources/MusicianResource.php
@@ -80,6 +80,7 @@ class MusicianResource extends Resource
                     ->default(),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
