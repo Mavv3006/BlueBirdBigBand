b73303275e0c32c58b1f94ead1f6ae0437d07936
Marvin Deuschle <marvdeus@gmail.com>
2023-12-02 22:46:58 +0100

    created ConcertResource.php and SongResource.php

---
+++ app/Filament/Resources/ConcertResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ConcertResource\Pages;
+use App\Models\Concert;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ConcertResource extends Resource
+{
+    protected static ?string $model = Concert::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListConcerts::route('/'),
+            'create' => Pages\CreateConcert::route('/create'),
+            'edit' => Pages\EditConcert::route('/{record}/edit'),
+        ];
+    }
+}
