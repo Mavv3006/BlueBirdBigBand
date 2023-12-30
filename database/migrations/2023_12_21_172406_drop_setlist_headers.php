10e3bdd7521ca65b0a214a889d2aab6c6f450a5e
Mavv3006 <marvdeus@gmail.com>
2023-12-30 22:03:23 +0100

    updated migrations to accommodate mariadb database (switch from sqlite to mariadb)

--- database/migrations/2023_12_21_172406_drop_setlist_headers.php
+++ database/migrations/2023_12_21_172406_drop_setlist_headers.php
@@ -4,10 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
+        Schema::table('setlist_entries', function (Blueprint $table) {
+            $table->dropForeign('setlist_entries_setlist_id_foreign');
+            $table->dropColumn('setlist_id');
+        });
+
         Schema::table('setlist_headers', function (Blueprint $table) {
             $table->drop();
         });
