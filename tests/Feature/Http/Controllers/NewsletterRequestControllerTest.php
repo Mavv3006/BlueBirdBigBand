eec04d5a313471e4977934d9e08c39125c049f4a
Mavv3006 <marvdeus@gmail.com>
2024-02-10 11:01:45 +0100

    fixed tests

--- tests/Feature/Http/Controllers/NewsletterRequestControllerTest.php
+++ tests/Feature/Http/Controllers/NewsletterRequestControllerTest.php
@@ -2,14 +2,27 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Enums\FeatureFlagName;
 use App\Enums\NewsletterType;
+use App\Enums\StateMachines\FeatureFlagState;
 use App\Enums\StateMachines\NewsletterState;
+use App\Models\FeatureFlag;
 use App\Models\NewsletterRequest;
+use App\Services\FeatureFlag\FeatureFlagService;
 use App\StateMachines\NewsletterRequest\RequestedNewsletterState;
 use Tests\TestCase;
 
 class NewsletterRequestControllerTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        FeatureFlag::factory()->create([
+            'name' => FeatureFlagName::Newsletter,
+            'status' => FeatureFlagState::On,
+        ]);
+    }
+
     public function testAddingRequest()
     {
         $this->assertDatabaseCount(NewsletterRequest::class, 0);
