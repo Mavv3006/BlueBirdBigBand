9a00a5a5457fca5955112fa3f678200070e5bfb6
Mavv3006 <marvdeus@gmail.com>
2024-02-12 20:52:27 +0100

    testing emails

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Mail\TestMail;
 use Filament\Actions;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Mail;
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +17,17 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Actions\Action::make('email')
+                ->label('E-Mail')
+                ->form([
+                    TextInput::make('email')
+                        ->label('E-Mail Adresse')
+                        ->email()
+                        ->required(),
+                ])
+                ->action(
+                    fn (array $data) => Mail::to($data['email'])->send(new TestMail())
+                ),
         ];
     }
 }
