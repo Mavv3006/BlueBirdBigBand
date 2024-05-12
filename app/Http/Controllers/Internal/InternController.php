f81f65b693a7940eb1633dcae2bf6f56b4d70d9d
Marvin Deuschle <marvdeus@gmail.com>
2024-05-12 13:24:10 +0200

    Added SeoMetaService.php to routes in controller

--- app/Http/Controllers/Internal/InternController.php
+++ app/Http/Controllers/Internal/InternController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Internal;
 
+use App\DataTransferObjects\SeoMetaDto;
 use App\Http\Controllers\Controller;
+use App\Services\SeoMetaService;
 use App\Services\Song\SongService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\RedirectResponse;
@@ -24,11 +26,21 @@ class InternController extends Controller
 
     public function emails(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'E-Mail Verteiler',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Intern/Emails');
     }
 
     public function songs(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Songs',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Intern/Songs', [
             'songs' => $this->songService->all(),
         ]);
--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTransferObjects\SeoMetaDto;
 use App\Services\Concert\ConcertService;
 use App\Services\Musician\MusicianService;
+use App\Services\SeoMetaService;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -17,21 +19,41 @@ class PublicController extends Controller
 
     public function home(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Willkommen',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Index');
     }
 
     public function aboutUs(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Über uns',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Band/AboutPage');
     }
 
     public function arrival(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Anfahrt',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Band/ArrivalPage');
     }
 
     public function concerts(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Auftritte',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('LatestInfos/ConcertsPage', [
             'concerts' => $this->concertService->upcoming(),
         ]);
@@ -39,26 +61,51 @@ class PublicController extends Controller
 
     public function booking(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Buchungsinfos',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('LatestInfos/BookingPage');
     }
 
     public function imprint(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Impressum',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Contact/ImprintPage');
     }
 
     public function contact(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Kontakt',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Contact/ContactPage');
     }
 
     public function pressInfo(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Presseinfos',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('LatestInfos/PressInfoPage');
     }
 
     public function musicians(): Response
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Musiker',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Band/MusiciansPage', [
             'data' => $this->musicianService->activeMusicians(),
         ]);
@@ -66,6 +113,11 @@ class PublicController extends Controller
 
     public function newsletter()
     {
+        SeoMetaService::setSeoMetaDto(new SeoMetaDto(
+            title: 'Newsletter',
+            description: 'Homepage der Blue Bird Big Band - hier finden sie Infos und die aktuellen Veranstaltungstermine der Blue Bird Bigband der Musikschule Speyer'
+        ));
+
         return Inertia::render('Contact/Newsletter');
     }
 }
