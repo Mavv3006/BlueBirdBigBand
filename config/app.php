6c764b446661049ef6c9d4e42de065c63be27025
Marvin Deuschle <marvdeus@gmail.com>
2024-05-13 16:38:11 +0200

    deactivated debugbar as default

--- config/app.php
+++ config/app.php
@@ -213,5 +213,5 @@ return [
 
     'use design v2' => env('APP_USE_DESIGN_V2', true),
 
-    'debugbar_enabled' => env('DEBUGBAR_ENABLED', true),
+    'debugbar_enabled' => env('DEBUGBAR_ENABLED', false),
 ];
