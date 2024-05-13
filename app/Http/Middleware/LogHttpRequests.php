a84a46aa1c2dcbcee0d4c78da51b7afdcbaff13c
Marvin Deuschle <marvdeus@gmail.com>
2024-05-13 16:01:59 +0200

    added new logging middleware

---
+++ app/Http/Middleware/LogHttpRequests.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogHttpRequests
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::debug('Request logged', [
+            'url' => $request->url(),
+            'method' => $request->method(),
+            'data' => $request->except('password'),
+        ]);
+
+        return $next($request);
+    }
+}
