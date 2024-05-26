f174296cde07e8bccdee4a943205a54bb6f04a3c
Mavv3006 <marvdeus@gmail.com>
2024-05-26 19:55:56 +0200

    changed return type of query function

--- app/Services/RepertoireService.php
+++ app/Services/RepertoireService.php
@@ -5,21 +5,18 @@ namespace App\Services;
 use App\Models\Concert;
 use App\Models\Song;
 use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Collection;
 
 class RepertoireService
 {
-    /**
-     * @return Song[]
-     */
-    public static function getCurrentRepertoire(int $limit = 5): array
+    public static function getCurrentRepertoire(int $limit = 5): Collection
     {
         return Song::join('setlist_entries', 'songs.id', '=', 'setlist_entries.song_id')
             ->joinSub(
                 Concert::limit($limit)->orderBy('date', 'desc'),
                 'concerts',
-                fn(JoinClause $join) => $join->on('setlist_entries.concert_id', '=', 'concerts.id')
+                fn (JoinClause $join) => $join->on('setlist_entries.concert_id', '=', 'concerts.id')
             )
-            ->groupBy('songs.id')
             ->get();
     }
 }
