2cae4fc7f880096d32c9d08af20cca1ad9f61965
Mavv3006 <marvdeus@gmail.com>
2024-05-26 19:49:19 +0200

    created the query

--- app/Services/RepertoireService.php
+++ app/Services/RepertoireService.php
@@ -2,15 +2,24 @@
 
 namespace App\Services;
 
+use App\Models\Concert;
 use App\Models\Song;
+use Illuminate\Database\Query\JoinClause;
 
 class RepertoireService
 {
     /**
      * @return Song[]
      */
-    public static function getCurrentRepertoire(): array
+    public static function getCurrentRepertoire(int $limit = 5): array
     {
-
+        return Song::join('setlist_entries', 'songs.id', '=', 'setlist_entries.song_id')
+            ->joinSub(
+                Concert::limit($limit)->orderBy('date', 'desc'),
+                'concerts',
+                fn(JoinClause $join) => $join->on('setlist_entries.concert_id', '=', 'concerts.id')
+            )
+            ->groupBy('songs.id')
+            ->get();
     }
 }
