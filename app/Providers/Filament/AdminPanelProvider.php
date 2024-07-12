b2676513ea1da62571113e3c9331a2f77757efb3
Mavv3006 <marvdeus@gmail.com>
2024-07-12 10:50:06 +0200

    removed unused widgets

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,8 +37,6 @@ class AdminPanelProvider extends PanelProvider
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-            ])
             ->navigationGroups([
                 NavigationGroup::make()
                     ->label('Auftritte'),
