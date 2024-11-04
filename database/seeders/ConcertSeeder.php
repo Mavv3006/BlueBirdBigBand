749b19965853016fd94455773bb4900442552467
Marvin Deuschle <marvdeus@gmail.com>
2024-11-04 20:26:17 +0100

    Refactor ConcertSeeder to introduce createConcerts method for improved date handling and increase the number of concerts generated from 25 to 100, with separate handling for future and past dates.

--- database/seeders/ConcertSeeder.php
+++ database/seeders/ConcertSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Concert;
-use Illuminate\Database\QueryException;
 use Illuminate\Database\Seeder;
 
 class ConcertSeeder extends Seeder
@@ -13,12 +12,28 @@ class ConcertSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 0; $i < 25; $i++) {
-            try {
-                Concert::factory()->create();
-            } catch (QueryException $e) {
-                print_r('('.$i.') Failed to create '.Concert::class."\nMessage: ".$e->getMessage()."\n\n");
-            }
+        $num = 50;
+        $this->createConcerts('-', $num);
+        $this->createConcerts('+', $num);
+    }
+
+    /**
+     * Creates a specified number of concert entries in the database with dates calculated
+     * based on the provided sign (either '+' for future dates or '-' for past dates).
+     *
+     * @param string $sign Indicates the direction of date calculation ('+' for future,
+     *                     '-' for past).
+     * @param int $num The number of concerts to create.
+     *
+     * @return void
+     */
+    protected function createConcerts(string $sign, int $num): void
+    {
+        $delta = 0;
+        for ($i = 0; $i < $num; $i++) {
+            $delta = $delta + rand(10, 30);
+            $date = date('Y-m-d', strtotime("$sign$delta days"));
+            Concert::factory()->create(['date' => $date]);
         }
     }
 }
