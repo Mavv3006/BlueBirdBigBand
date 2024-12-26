c15c10c24c245b085421c2bfa11c82dc73266561
Marvin Deuschle <marvdeus@gmail.com>
2024-12-26 12:01:18 +0100

    added test

---
+++ tests/Inertia/Public/DataPrivacyTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Public;
+
+use Inertia\Testing\AssertableInertia;
+use Tests\TestCase;
+
+class DataPrivacyTest extends TestCase
+{
+    public function test_route_returns_successful()
+    {
+        $this->get('/datenschutz')
+            ->assertSuccessful();
+    }
+
+    public function test_correct_view()
+    {
+        $this->get('/datenschutz')
+            ->assertInertia(
+                fn (AssertableInertia $page) => $page
+                    ->component('Contact/DataPrivacyPage')
+            );
+    }
+}
