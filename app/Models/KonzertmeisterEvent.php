5523cdf3602e2c313208c6b9e93361ceff78ff3f
Marvin Deuschle <marvdeus@gmail.com>
2024-12-30 20:58:08 +0100

    create a Filament Resource for KonzertmeisterEvents

--- app/Models/KonzertmeisterEvent.php
+++ app/Models/KonzertmeisterEvent.php
@@ -8,6 +8,7 @@ use App\StateMachines\KonzertmeisterEvent\BaseKonzertmeisterEventConversionState
 use App\StateMachines\KonzertmeisterEvent\ConvertedKonzertmeisterEventConversionState;
 use App\StateMachines\KonzertmeisterEvent\OpenKonzertmeisterEventConversionState;
 use App\StateMachines\KonzertmeisterEvent\RejectedKonzertmeisterEventConversionState;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -16,6 +17,13 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class KonzertmeisterEvent extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'band_id',
+        'description',
+        'conversion_state',
+    ];
 
     protected $casts = [
         'type' => KonzertmeisterEventType::class,
@@ -34,12 +42,27 @@ class KonzertmeisterEvent extends Model
         return $this->belongsTo(Band::class);
     }
 
-    public function conversionState(): BaseKonzertmeisterEventConversionState
+    public function state(): BaseKonzertmeisterEventConversionState
     {
         return match ($this->conversion_state) {
             KonzertmeisterEventConversionState::Converted => new ConvertedKonzertmeisterEventConversionState($this),
             KonzertmeisterEventConversionState::Open => new OpenKonzertmeisterEventConversionState($this),
             KonzertmeisterEventConversionState::Rejected => new RejectedKonzertmeisterEventConversionState($this),
         };
+    }
+
+    protected function description(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $value,
+            set: fn (string $value) => self::shortenDescription($value),
+        );
+    }
+
+    public static function shortenDescription(string $value): string
+    {
+        return strlen($value) > 125
+            ? substr($value, 0, 125 - 3).'...'
+            : substr($value, 0, 125 - 3);
     }
 }
