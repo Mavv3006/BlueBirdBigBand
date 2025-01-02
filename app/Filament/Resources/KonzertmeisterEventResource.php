ace06abeb724378e58fab47f19d8ab44c63daef4
Marvin Deuschle <marvdeus@gmail.com>
2025-01-02 12:53:52 +0100

    changed KonzertmeisterEventResource navigation icon

--- app/Filament/Resources/KonzertmeisterEventResource.php
+++ app/Filament/Resources/KonzertmeisterEventResource.php
@@ -20,7 +20,7 @@ class KonzertmeisterEventResource extends Resource
 {
     protected static ?string $model = KonzertmeisterEvent::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'icon-event-o';
 
     protected static ?string $navigationGroup = 'Auftritte';
 
