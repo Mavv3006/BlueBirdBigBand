65578a167b9529a37e51a21db9673e23cfbd22dc
Marvin Deuschle <marvdeus@gmail.com>
2025-02-08 14:06:45 +0100

    fixed email address

--- app/Mail/NewsletterAdminNotificationMail.php
+++ app/Mail/NewsletterAdminNotificationMail.php
@@ -29,7 +29,7 @@ class NewsletterAdminNotificationMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            to: 'webadmin@bluebirdbigband.com',
+            to: 'webadmin@bluebirdbigband.de',
             subject: match ($this->newsletterRequest->type) {
                 NewsletterType::Adding => '[BBBB] Neue Newsletter-Anfrage - Hinzufügen',
                 NewsletterType::Removing => '[BBBB] Neue Newsletter-Anfrage - Entfernen',
