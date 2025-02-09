221efc46cfbe946dfdf88dcc5b11da4be0303728
Marvin Deuschle <marvdeus@gmail.com>
2025-02-09 11:34:22 +0100

    added short explanation regarding expiration of link in confirmation email

--- resources/views/mail/newsletter-confirmation.blade.php
+++ resources/views/mail/newsletter-confirmation.blade.php
@@ -11,12 +11,16 @@ Um Ihre Anmeldung abzuschließen, klicken Sie bitte auf den untenstehenden Link:
     E-Mail Adresse bestätigen
 </x-mail::button>
 
+Bitte beachten Sie, dass der Bestätigungslink nur 24 Stunden gültig ist. Falls Sie die Anmeldung nicht innerhalb
+dieser Frist abschließen, müssen Sie sich erneut registrieren.
+
 Durch Klicken auf den Link bestätigen Sie, dass Sie unseren Newsletter erhalten möchten. Sollten Sie sich nicht für
-unseren Newsletter angemeldet haben oder diese E-Mail versehentlich erhalten haben, können Sie sie einfach ignorieren.
+unseren Newsletter angemeldet haben oder diese E-Mail versehentlich erhalten haben, können Sie sie einfach
+ignorieren.
 
 Wir freuen uns darauf, Sie in unserem Newsletter begrüßen zu dürfen und Sie über unsere anstehenden Konzerte auf dem
 Laufenden zu halten.
 
 Vielen Dank und herzliche Grüße,<br>
 Blue Bird Big Band
-</x-mail::message>>
+</x-mail::message>
