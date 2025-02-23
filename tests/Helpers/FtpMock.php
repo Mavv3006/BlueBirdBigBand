8774d5090b0e25556b89531c6792582fe5ec3b8f
Mavv3006 <Mavv3006@users.noreply.github.com>
2025-02-23 08:11:23 +0000

    PHP Linting (Pint)

--- tests/Helpers/FtpMock.php
+++ tests/Helpers/FtpMock.php
@@ -2,26 +2,32 @@
 
 namespace App\Console\Commands;
 
-function ftp_connect($server) {
+function ftp_connect($server)
+{
     return true; // Immer eine "erfolgreiche" Verbindung simulieren
 }
 
-function ftp_login($conn, $username, $password) {
+function ftp_login($conn, $username, $password)
+{
     return true;
 }
 
-function ftp_put($conn, $remoteFile, $localFile, $mode) {
+function ftp_put($conn, $remoteFile, $localFile, $mode)
+{
     return true; // Erfolgreiches Hochladen simulieren
 }
 
-function ftp_delete($conn, $remoteFile) {
+function ftp_delete($conn, $remoteFile)
+{
     return true; // Erfolgreiches Löschen simulieren
 }
 
-function ftp_close($conn) {
+function ftp_close($conn)
+{
     return true;
 }
 
-function ftp_pasv($conn, $mode) {
+function ftp_pasv($conn, $mode)
+{
     return true; // Erfolgreichen Passive Mode simulieren
 }
