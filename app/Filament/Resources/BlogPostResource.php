3e00480f99bb77067b0d45a0bf1a24d06e9b3dfc
Marvin Deuschle <marvdeus@gmail.com>
2025-08-21 16:24:02 +0200

    started with creating logic for creating blog posts

---
+++ app/Filament/Resources/BlogPostResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BlogPostResource\Pages;
+use App\Models\BlogPost;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class BlogPostResource extends Resource
+{
+    protected static ?string $model = BlogPost::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Grid::make(1)->schema([
+
+                    Forms\Components\TextInput::make('title')
+                        ->label('Titel des Posts')
+                        ->required(),
+                    Forms\Components\RichEditor::make('content')
+                        ->label('Post Inhalt')
+                        ->fileAttachmentsDisk('public')
+                        ->fileAttachmentsDirectory('blog-post/images')
+                        ->required(),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID'),
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Titel'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Erstelldatum')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBlogPosts::route('/'),
+            'create' => Pages\CreateBlogPost::route('/create'),
+            'edit' => Pages\EditBlogPost::route('/{record}/edit'),
+            'view' => Pages\ViewBlogPost::route('/{record}'),
+        ];
+    }
+}
