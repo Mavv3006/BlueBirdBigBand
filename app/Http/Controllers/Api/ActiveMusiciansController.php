f5cb598f6229efc4a4c08a5fb89681d2219fad4d
Marvin Deuschle <marvdeus@gmail.com>
2026-01-11 13:52:13 +0100

    initial creation of controllers and routes

---
+++ app/Http/Controllers/Api/ActiveMusiciansController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\Musician\MusicianService;
+
+class ActiveMusiciansController extends Controller
+{
+
+    public function __construct(public MusicianService $service)
+    {
+    }
+
+    public function __invoke()
+    {
+        return $this->service->activeMusicians();
+    }
+}
---
+++ app/Http/Controllers/Api/UpcomingConcertsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\Concert\ConcertService;
+
+class UpcomingConcertsController extends Controller
+{
+
+    public function __construct(public ConcertService $service )
+    {    }
+
+    public function __invoke()
+    {
+        return $this->service->upcoming();
+    }
+}
