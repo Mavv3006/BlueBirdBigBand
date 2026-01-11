70aec3f86075c867c4bd8c0ea601c119f643c846
Marvin Deuschle <marvdeus@gmail.com>
2026-01-11 13:52:13 +0100

    initial creation of controllers and routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ActiveMusiciansController;
 use App\Http\Controllers\Api\KonzertmeisterUpdateConcertsController;
+use App\Http\Controllers\Api\UpcomingConcertsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('concerts/pull', KonzertmeisterUpdateConcertsController::class)->name('api.concerts.pull');
+
+Route::get('concerts/upcoming', UpcomingConcertsController::class);
+
+Route::get('musicians/active', ActiveMusiciansController::class);
