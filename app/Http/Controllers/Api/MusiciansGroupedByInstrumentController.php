04ca9ddfac416874151635950dc98992a069b90b
Mavv3006 <marvdeus@gmail.com>
2026-02-14 16:13:51 +0100

    added tests for api.musicians.grouped-by-instrument

--- app/Http/Controllers/Api/MusiciansGroupedByInstrumentController.php
+++ app/Http/Controllers/Api/MusiciansGroupedByInstrumentController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\InstrumentResource;
+use App\Models\Instrument;
 use App\Services\Musician\MusicianService;
 
 class MusiciansGroupedByInstrumentController extends Controller
 {
-    public function __construct(public MusicianService $service) {}
-
     public function __invoke()
     {
-        return $this->service->activeMusicians();
+        $instruments = Instrument::with(['musicians' => function ($query) {
+            $query->where('isActive', true)
+                ->orderBy('firstname')
+                ->orderBy('lastname');
+        }])->orderBy('order')
+            ->get();
+
+        return InstrumentResource::collection($instruments);
     }
 }
